27f6eba9965524fc2333dc49137fc318537f52ca
thiago <thiagomellodiniz@hotmail.com>
2025-10-30 14:15:58 -0400

    feat: criando CRUD básico;

---
+++ src/users/users.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Users {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({length: 200})
+    firstName: string;
+
+    @Column({length: 200})
+    lastName: string;
+
+    @Column({
+        unique: true
+    })
+    email: string;
+}
